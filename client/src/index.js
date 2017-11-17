d365632ce71afb8d19931e6a41b4beb77857eb39
Damian Stolarek <damian.s.ja@gmail.com>
2017-11-17 18:29:13 +0100

    Added redux store to application

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
+import reducers from './reducers';
 import App from './components/App';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware()
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
