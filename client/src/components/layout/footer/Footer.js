2b9e4693d00f875e95b13f612d0137ef587eaf4d
Damian Stolarek <damian.s.ja@gmail.com>
2017-11-19 17:33:03 +0100

    Fixed footer styles

--- client/src/components/layout/footer/Footer.js
+++ client/src/components/layout/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = props => (
-  <footer className="footer bg-dark mt-5">
+  <footer className="footer bg-dark">
     <div className="container">
       <span className="text-light">Place sticky footer content here.</span>
     </div>
