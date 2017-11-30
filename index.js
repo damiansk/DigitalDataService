b2a6a765e708b96d0d1cb49ebebc35210e177303
Damian Stolarek <damian.s.ja@gmail.com>
2017-11-30 05:39:23 +0100

    Added bodyParser and morgan middlewares for express server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
+
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+
 
 const PORT = process.env.PORT || 5000;
 
