f052de3fa73564fefb48574dbec635f04cfa3681
Damian Stolarek <damian.s.ja@gmail.com>
2017-12-01 18:40:29 +0100

    Added simple connection to database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const router = require('./router');
 const app = express();
 
+
+mongoose.connect('mongodb://localhost/digitaldata');
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
