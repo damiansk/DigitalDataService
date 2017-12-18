6a194a874e791fef04048efaa55e257f9f0bcb4d
Damian Stolarek <damian.s.ja@gmail.com>
2017-12-18 19:00:37 +0100

    Added action for fetch record data (currently with mocked req & res)

--- client/src/actions/records.js
+++ client/src/actions/records.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-import { RECORDS_FETCH_USER_RECORDS, RECORDS_SAVE_RECORD, UNAUTH_USER } from '../constants/actions';
+import {
+  RECORDS_FETCH_USER_RECORDS,
+  RECORDS_SAVE_RECORD,
+  RECORD_FETCH,
+  UNAUTH_USER
+} from '../constants/actions';
 import { ADD_RECORD, USER_RECORDS } from '../constants/api';
 
 
@@ -11,7 +16,9 @@ export function saveRecord(record) {
     if(!token || token === '') {
       return dispatch({type: UNAUTH_USER});
     }
-    
+  
+    console.log(record);
+  
     const {
       files: models = [],
       ...restRecordData
@@ -42,6 +49,17 @@ export function saveRecord(record) {
       }
     });
   };
+}
+
+export function fetchRecord(recordId) {
+  return dispatch => {
+    const mock = "{\"title\":\"Super Title 11121212\",\"resourceType\":\"Type of resource \",\"keywords\":\"Manga itp itd\",\"destination\":\"Students\",\"description\":\"Super simple description about this record\",\"files\":[{\"file\":{\"preview\":\"blob:http://localhost:3000/1d26ee20-c865-4a53-bc6d-a78b19c9fdfe\"},\"name\":\"Heart.obj\",\"description\":\"Auto generated description for Heart.obj...\",\"thumbnail\":\"\"},{\"file\":{\"preview\":\"blob:http://localhost:3000/47f0014f-1d0f-411d-868e-6fcd614506b8\"},\"name\":\"deer.obj\",\"description\":\"Auto generated description for deer.obj...\",\"thumbnail\":\"\"}]}";
+    
+    dispatch({
+      type: RECORD_FETCH,
+      payload: JSON.parse(mock)
+    })
+  }
 }
 
 export function fetchUserRecords() {
--- client/src/reducers/records.js
+++ client/src/reducers/records.js
@@ -1,13 +1,19 @@
-import { RECORDS_FETCH_USER_RECORDS } from '../constants/actions'
+import {
+  RECORDS_FETCH_USER_RECORDS,
+  RECORD_FETCH
+} from '../constants/actions';
 
 const initialState = {
-  userRecords: []
+  userRecords: [],
+  fetchedRecord: {}
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case RECORDS_FETCH_USER_RECORDS:
       return {...state, userRecords: action.payload};
+    case RECORD_FETCH:
+      return {...state, fetchedRecord: action.payload};
     default:
       return state;
   }
