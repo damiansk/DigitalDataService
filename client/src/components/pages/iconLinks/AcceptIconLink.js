562a210d07df4925a4fbba49a76b39790cdcce9c
Damian Stolarek <damian.s.ja@gmail.com>
2017-12-29 10:02:25 +0100

    Updated AcceptIconLink (icon) and fixed missing Icons in index file

--- client/src/components/pages/iconLinks/AcceptIconLink.js
+++ client/src/components/pages/iconLinks/AcceptIconLink.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const AcceptIconLink = ({recordId}) => (
-  <Link className="nav-link text-secondary" to={`/record/edit/${recordId}`}>
-    <i className={`fa fa-paper-plane-o fa-1x`} aria-hidden="true"/>
+  <Link className="nav-link text-secondary" to={`/record/update_status/${recordId}/accept`}>
+    <i className={`fa fa-check-square-o fa-1x`} aria-hidden="true"/>
   </Link>
 );
 
--- client/src/components/pages/iconLinks/index.js
+++ client/src/components/pages/iconLinks/index.js
@@ -1,5 +1,13 @@
 import PreviewIconLink from './PreviewIconLink';
 import EditIconLink from './EditIconLink';
+import ReportIconLink from './ReportIconLink';
 import AcceptIconLink from './AcceptIconLink';
+import RejectIconLink from './RejectIconLink';
 
-export {PreviewIconLink, AcceptIconLink, EditIconLink};
+export {
+  PreviewIconLink,
+  ReportIconLink,
+  EditIconLink,
+  AcceptIconLink,
+  RejectIconLink
+};
