a6fdf2b29e1966ec5d8000b8bee12a48ae3152c2
Damian Stolarek <damian.s.ja@gmail.com>
2018-01-06 18:58:00 +0100

    Updated actions for new and reported records

--- client/src/components/pages/iconLinks/iconLinksGroup/NewRecordsActions.js
+++ client/src/components/pages/iconLinks/iconLinksGroup/NewRecordsActions.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { PreviewIconLink, EditIconLink, ReportIconLink } from '..';
+import { PreviewIconLink, EditIconLink, ReportIconLink, DeleteIconLink } from '..';
 
 const NewRecordsActions = ({id}) => (
   <ul className="nav justify-content-center">
     <li className="nav-item"><PreviewIconLink recordId={id}/></li>
     <li className="nav-item"><EditIconLink recordId={id}/></li>
     <li className="nav-item"><ReportIconLink recordId={id}/></li>
+    <li className="nav-item"><DeleteIconLink recordId={id}/></li>
   </ul>
 );
 
--- client/src/components/pages/iconLinks/iconLinksGroup/ReportedRecordsActions.js
+++ client/src/components/pages/iconLinks/iconLinksGroup/ReportedRecordsActions.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { PreviewIconLink, AcceptIconLink, RejectIconLink } from '..';
+import { PreviewIconLink, AcceptIconLink, RejectIconLink, DeleteIconLink } from '..';
 
 const ReportedRecordsActions = ({id}) => (
   <ul className="nav justify-content-center">
     <li className="nav-item"><AcceptIconLink recordId={id}/></li>
     <li className="nav-item"><RejectIconLink recordId={id}/></li>
     <li className="nav-item"><PreviewIconLink recordId={id}/></li>
+    <li className="nav-item"><DeleteIconLink recordId={id}/></li>
   </ul>
 );
 
