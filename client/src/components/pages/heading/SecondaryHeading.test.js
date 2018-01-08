04817df3ad51024c38d0630206650dbd1aae1ded
Damian Stolarek <damian.s.ja@gmail.com>
2018-01-08 07:54:48 +0100

    Added SecondaryHeading test

---
+++ client/src/components/pages/heading/SecondaryHeading.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import SecondaryHeading from './SecondaryHeading';
+
+describe('<SecondaryHeading/>', () => {
+  
+  it('renders without crashing', () => {
+    mount(<SecondaryHeading title="Test"/>);
+  });
+  
+  it('contains correct tag elements', () => {
+    const wrapper = shallow(<SecondaryHeading title="Test"/>);
+    
+    expect(wrapper.find('header')).toBeTruthy();
+    expect(wrapper.find('h3')).toBeTruthy();
+  });
+  
+  it('render right text', () => {
+    const headerText = 'Test';
+    const wrapper = shallow(<SecondaryHeading title={headerText}/>);
+    
+    expect(wrapper.find('h3').html()).toContain(headerText)
+  });
+  
+  it('added passed class to header tag', () => {
+    const testClass = 'test';
+    const wrapper = shallow(<SecondaryHeading className={testClass} title="Tets"/>);
+    
+    expect(wrapper.find(`header.${testClass}`)).toBeTruthy();
+  });
+  
+});
